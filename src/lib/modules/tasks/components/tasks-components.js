aa8adc0ef162c64eb304c211ff637e8a4acceb71
msalvetat <marjorie.salvetat@gmail.com>
2018-10-05 16:08:34 +0200

    build(registration): Stop custom element in dev

--- src/lib/modules/tasks/components/tasks-components.js
+++ src/lib/modules/tasks/components/tasks-components.js
@@ -15,24 +15,11 @@ const tasksComponents = [
 
 export default {
   install: (Vue, options) => {
-    if (!window.registredAerisElements) {
-      window.registredAerisElements = [];
-    }
-
-    let timer = setInterval(function() {
-      if (
-        window.registredAerisElements.indexOf("aeris-commons-components-vjs") >
-        -1
-      ) {
-        Vue.use(tasksComponents, {
-          store: options.store
-        });
-        HelperRegisterModule.registerStore(options.store, tasksComponents);
-        HelperRegisterModule.registerVueComponents(Vue, tasksComponents);
-        HelperRegisterModule.registerCustomElements(Vue, tasksComponents);
-
-        clearInterval(timer);
-      }
-    }, 1000);
+    Vue.use(tasksComponents, {
+      store: options.store
+    });
+    HelperRegisterModule.registerStore(options.store, tasksComponents);
+    HelperRegisterModule.registerVueComponents(Vue, tasksComponents);
+    HelperRegisterModule.registerCustomElements(Vue, tasksComponents);
   }
 };
